f0a432656049e13f4ff67fe4e6f4a54c08f991dc
Raissa Schmitt <raissajoaquim29@gmail.com>
2025-10-14 18:06:01 -0300

    adicionando descrição no test

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,9 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import Greeting from "./Greeting";
 
-test('renders hello world as a text', () => {
-    render(<Greeting />);
+describe('<Greeting />', () => {
+    test('renders hello world as a text', () => {
+        render(<Greeting />);
 
-    const helloWorldElement = screen.getByText('hello world');
-    expect(helloWorldElement).toBeInTheDocument();
+        const helloWorldElement = screen.getByText('hello world!');
+        expect(helloWorldElement).toBeInTheDocument();
+    });
 });
+
+
