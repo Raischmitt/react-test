2ec6d8fd45dee39a4456f5bc99eef2f03f37f2d4
Raissa Schmitt <raissajoaquim29@gmail.com>
2025-10-15 19:25:55 -0300

    criando mais testes

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event;'
 import Greeting from "./Greeting";
 
 describe('<Greeting />', () => {
@@ -7,6 +8,33 @@ describe('<Greeting />', () => {
 
         const helloWorldElement = screen.getByText('hello world!');
         expect(helloWorldElement).toBeInTheDocument();
+    });
+
+    test('renders good to see you if the button was NOT clicked', () => {
+        render(<Greeting />);
+
+        const outputElement = screen.getByText(' good to see you', { exact: false });
+        expect(outputElement).toBeInTheDocument();
+    });
+
+    test('renders changed! if the button was clicked', () => {
+        render(<Greeting />);
+
+        const buttonElement = screen.getByRole('button');
+        userEvent.click(buttonElement);
+
+        const outputElement = screen.getByText(' changed');
+        expect(outputElement).toBeInTheDocument();
+    })
+
+    test('does not render good to see you if the button wsa clicked', () => {
+        render(<Greeting />);
+
+        const buttonElement = screen.getByRole('button');
+        userEvent.click(buttonElement);
+
+        const outputElement = screen.queryByText('good to see you', { exact: false});
+        expect(outputElement).toBeNull();
     });
 });
 
